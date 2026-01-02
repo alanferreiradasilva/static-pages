9dc4076bb9033202bfd8ecf6783da4ef51af6716
alanferreiradasilva <alanferreiradasilva@gmail.com>
2026-01-02 13:33:20 -0300

    Initial commit.

---
+++ cars-list/cars-consts.js
@@ -0,0 +1,32 @@
+const carsData = [
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaChJ0or-C3nlRXswiAZcGsDokqnZ5_Ih8Cw&s',
+        name: 'Chevrolet Equinox Premier 2.0',
+        link: 'https://www.webmotors.com.br/carros/estoque/chevrolet/equinox/20-16v-turbo-gasolina-premier-awd-automatico?tipoveiculo=carros&marca1=CHEVROLET&modelo1=EQUINOX&versao1=2.0%2016V%20TURBO%20GASOLINA%20PREMIER%20AWD%20AUTOM%C3%81TICO&o=5'
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVT5EuHoJKUvUJ0fmPQkvg_E8L6xcWFJ7l9w&s',
+        name: 'Peugeot 3008 Griffe Pack',
+        link: 'https://www.webmotors.com.br/carros/estoque/peugeot/3008/16-griffe-pack-thp-16v-gasolina-4p-automatico?tipoveiculo=carros&marca1=PEUGEOT&modelo1=3008&versao1=1.6%20GRIFFE%20PACK%20THP%2016V%20GASOLINA%204P%20AUTOM%C3%81TICO&o=5'
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu7-6qIzi7dImIyORVC97Ess1MQyBJDZRbdQ&s',
+        name: 'Peugeot 5008 Griffe Pack',
+        link: 'https://www.webmotors.com.br/carros/estoque/peugeot/5008/16-griffe-pack-thp-16v-gasolina-4p-automatico?tipoveiculo=carros&marca1=PEUGEOT&modelo1=5008&versao1=1.6%20GRIFFE%20PACK%20THP%2016V%20GASOLINA%204P%20AUTOM%C3%81TICO&o=5'
+    },
+    {
+        image: 'https://revistacarro.com.br/wp-content/uploads/2024/02/Tiggo-7-Sport-6.png',
+        name: 'Tiggo 7 Sport',
+        link: 'https://www.webmotors.com.br/carros/rj/caoa-chery/tiggo-7/15-tci-turbo-flex-sport-cvt?estadocidade=Rio%20de%20Janeiro&tipoveiculo=carros&marca1=CAOA%20CHERY&modelo1=TIGGO%207&versao1=1.5%20TCI%20TURBO%20FLEX%20SPORT%20CVT&o=5'
+    },
+    {
+        image: 'https://cdn.motor1.com/images/mgl/Pp1qA/s1/caoa-chery-tiggo-8-2021-brasil.jpg',
+        name: 'Tiggo 8',
+        link: 'https://www.webmotors.com.br/carros/estoque/caoa-chery/tiggo-8?autocomplete=tiggo%208&autocompleteTerm=Caoa%20Chery%20Tiggo%208&tipoveiculo=carros&marca1=CAOA%20CHERY&modelo1=TIGGO%208&o=5&page=1'
+    },
+    {
+        image: 'https://s3.ecompletocarros.dev/images/lojas/171/veiculos/209032/veiculoInfoVeiculoImagesMobile/vehicle_image_1725914979_707f4e51f80b5c96bb694be404aeda7c.jpeg',
+        name: 'Fiat Fastback Limited Edition',
+        link: 'https://www.webmotors.com.br/carros/estoque/fiat/fastback/13-turbo-270-flex-limited-edition-at6?tipoveiculo=carros&marca1=FIAT&modelo1=FASTBACK&versao1=1.3%20TURBO%20270%20FLEX%20LIMITED%20EDITION%20AT6&o=5'
+    }
+];
